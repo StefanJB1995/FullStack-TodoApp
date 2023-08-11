3c68b6a44e87459512bc8c773bf81a564698a428
Stefan John Bosco <johnboscostefan@gmail.com>
2023-08-11 13:00:44 -0400

    backend project setup

---
+++ backend/todo-web-service/todo-web-service/src/test/java/com/udemy/projects/todowebservice/TodoWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.projects.todowebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
