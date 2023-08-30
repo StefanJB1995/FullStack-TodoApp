119bbfdfa46e5a495dba0475589ef03c44014a63
Stefan John Bosco <johnboscostefan@gmail.com>
2023-08-30 15:27:10 -0400

    Path-Variable example

--- backend/todo-web-service/todo-web-service/src/main/java/com/udemy/projects/todowebservice/HelloWorldController.java
+++ backend/todo-web-service/todo-web-service/src/main/java/com/udemy/projects/todowebservice/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.udemy.projects.todowebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,16 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "Hello World";
 	}
-	
-	//hello-world-bean (we want toi return a Bean
+
+	// hello-world-bean (we want toi return a Bean)
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
 	}
 
+	// hello-world-bean (we want toi return a Bean with path-variable
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
 }
