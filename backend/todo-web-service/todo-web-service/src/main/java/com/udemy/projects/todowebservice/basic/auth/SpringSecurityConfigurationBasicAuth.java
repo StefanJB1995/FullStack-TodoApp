d0049d4ea6a47a883ed9e6d5261ef15126ba4c21
Stefan John Bosco <johnboscostefan@gmail.com>
2023-09-18 13:56:17 -0400

    config not working

--- backend/todo-web-service/todo-web-service/src/main/java/com/udemy/projects/todowebservice/basic/auth/SpringSecurityConfigurationBasicAuth.java
+++ backend/todo-web-service/todo-web-service/src/main/java/com/udemy/projects/todowebservice/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -13,7 +13,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SpringSecurityConfigurationBasicAuth{
     
-    @Bean
+    @SuppressWarnings("removal")
+	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //1: Response to preflight request doesn't pass access control check
         //2: basic auth
