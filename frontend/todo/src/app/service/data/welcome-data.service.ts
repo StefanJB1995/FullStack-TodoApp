cc370002793b5a0ec5a595f0632aa61415d49bc8
Stefan John Bosco <johnboscostefan@gmail.com>
2023-09-18 13:37:07 -0400

    springSecurity

--- frontend/todo/src/app/service/data/welcome-data.service.ts
+++ frontend/todo/src/app/service/data/welcome-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 export class HelloWorldBean {
   constructor(public message: string) { }
@@ -18,8 +18,22 @@ export class WelcomeDataService {
   }
 
   //http://localhost:8080/hello-world/path-variable/stef
-  executeHelloWorldServiceWithPathVariable(name : any) {
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`);
+  executeHelloWorldServiceWithPathVariable(name: any) {
+    let basicAuthHeaderString = this.createBasicAuthHttpHeader();
 
+    let header = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+    }
+    )
+    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`,{headers : header});
+
+  }
+
+  createBasicAuthHttpHeader() {
+    let username = 'stef'
+    let password = 'abc'
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    return basicAuthHeaderString;
   }
 }
